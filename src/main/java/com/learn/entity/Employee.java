e8e212897621a06116c64133327c50bb7fb6505a
Suckscorpio999 <olinabin214@gmail.com>
2026-03-15 11:26:37 +0545

    feat: add Employee entity for managing employee data

---
+++ src/main/java/com/learn/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.learn.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true , nullable = false)
+    private String email;
+
+    private String password;
+    private String department;
+    private double salary;
+    private boolean active;
+
+}
