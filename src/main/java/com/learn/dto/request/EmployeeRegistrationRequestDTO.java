38aeb4591a34339260e005d0c4b83e0485469368
Suckscorpio999 <olinabin214@gmail.com>
2026-03-15 11:26:42 +0545

    feat: add EmployeeRegistrationRequestDTO for employee registration data

---
+++ src/main/java/com/learn/dto/request/EmployeeRegistrationRequestDTO.java
@@ -0,0 +1,29 @@
+package com.learn.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class EmployeeRegistrationRequestDTO{
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6 , max = 18)
+    private String password;
+
+    private String department;
+
+    private double salary;
+
+}
