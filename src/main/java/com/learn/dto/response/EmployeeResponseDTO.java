be762221ed9d8ed56a2f145a8726da1b58fb993e
Suckscorpio999 <olinabin214@gmail.com>
2026-03-16 08:58:51 +0545

    feat: add EmployeeResponseDTO for employee response data representation

--- src/main/java/com/learn/dto/response/EmployeeResponseDTO.java
+++ src/main/java/com/learn/dto/response/EmployeeResponseDTO.java
@@ -1,4 +1,15 @@
 package com.learn.dto.response;
 
+import lombok.Data;
+
+@Data
 public class EmployeeResponseDTO {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String department;
+    private double salary;
+    private boolean active;
+
 }
