b5d3f8b941db019078c7d2fa442e3e29bfd6ace2
Suckscorpio999 <olinabin214@gmail.com>
2026-03-16 08:58:56 +0545

    feat: add EmployeeSummaryDTO for employee summary data representation

--- src/main/java/com/learn/dto/response/EmployeeSummaryDTO.java
+++ src/main/java/com/learn/dto/response/EmployeeSummaryDTO.java
@@ -1,4 +1,11 @@
 package com.learn.dto.response;
 
+import lombok.Data;
+
+@Data
 public class EmployeeSummaryDTO {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String department;
 }
