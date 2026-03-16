6905f6e2ddb1efa2dc78fcbe9336c00e2d5a958d
Suckscorpio999 <olinabin214@gmail.com>
2026-03-16 08:59:01 +0545

    feat: add EmployeeUpdateRequestDTO for employee update data representation

--- src/main/java/com/learn/dto/request/EmployeeUpdateRequestDTO.java
+++ src/main/java/com/learn/dto/request/EmployeeUpdateRequestDTO.java
@@ -1,4 +1,16 @@
 package com.learn.dto.request;
 
+import lombok.Data;
+
+@Data
 public class EmployeeUpdateRequestDTO {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String department;
+
+    private double salary;
 }
