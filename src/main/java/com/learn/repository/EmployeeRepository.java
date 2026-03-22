992288732790f80a49a69ed6b1bb242a9bc7361e
Suckscorpio999 <olinabin214@gmail.com>
2026-03-22 09:19:27 +0545

    feat: add custom query methods in EmployeeRepository

--- src/main/java/com/learn/repository/EmployeeRepository.java
+++ src/main/java/com/learn/repository/EmployeeRepository.java
@@ -4,9 +4,15 @@ import com.learn.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee , Long> {
     Optional<Employee> findByEmail(String email);
+
+    List<Employee> findByDepartmentName(String departmentName);
+    List<Employee> findBySalaryGreaterThan(double salary);
+    List<Employee> findByActiveTrue();
+    List<Employee> findByFirstNameContainingIgnoreCase(String keyword);
 }
